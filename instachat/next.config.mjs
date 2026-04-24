95229e297f843c89f61a000ec8e3f8964225a45f
Test User <test@example.com>
2026-04-24 19:04:12 +0530

    fix: add image remotePatterns for gstatic.com

--- instachat/next.config.mjs
+++ instachat/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.gstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
